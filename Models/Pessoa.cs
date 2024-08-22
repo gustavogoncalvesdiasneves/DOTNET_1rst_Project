c922f9fb474bc4b332fdde261d05c01f7e4cdf77
Gustavo Gonçalves <ggddnn71@gmail.com>
2024-08-22 20:40:40 -0300

    style: cases added

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 
 namespace DotNet_1RST_Project.Models
 {
-    public class Pessoa {
-        public string? Nome { get; set; }
-        public string? Sobrenome { get; set; }
-        public int? Idade { get; set; }
+    public class Pessoa // PascalCase
+    {
+        public string? Nome { get; set; } // PascalCase
+        public string? Sobrenome { get; set; } // PascalCase
+        public int? Idade { get; set; } // PascalCase
 
-        public void Apresentar()
+        public void Apresentar() // PascalCase
         {
             Console.WriteLine($"Olá, meu nome é {Nome} {Sobrenome}, e tenho {Idade} Anos.");
         }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using DotNet_1RST_Project.Models;
 
-Pessoa pessoa1 = new();
+Pessoa pessoa1 = new(); // camelCase ex: pessoaFisica
 
 pessoa1.Nome = "Gustavo";
 pessoa1.Sobrenome = "Gonçalves";
