f50e72bdd6abeaaf39db2f78ffba51956d7f81f2
Gustavo Gonçalves <ggddnn71@gmail.com>
2024-08-23 21:24:05 -0300

    feat: how to ConvertTypes, how to use swithCase, tabuadaX using For, doWhile and Menu

--- Program.cs
+++ Program.cs
@@ -11,8 +11,120 @@ void DateTimeHowToUse(){
     dateTimeUse.OnlyTime(); // mostra somente horas
 }
 
-DateTimeHowToUse();
+void ConvertTypes(){
+    int string5 = int.Parse("5");
+    int string6 = Convert.ToInt32("5");
+    int stringNull = Convert.ToInt32(null); // 0
 
+    Console.WriteLine($"string5: {string5}");
+    Console.WriteLine($"string6: {string6}");
+    Console.WriteLine($"stringNull: {stringNull}");
+
+    int inteiro = 5;
+    string a = inteiro.ToString();
+    Console.WriteLine($"inteiro.toString(): {a}");
+
+    // Implicity cast
+    int b = 5;
+    double c = b; // = 5
+    Console.WriteLine($"c: {c}");
+
+    // tryParse
+    string d = "15-";
+    int e = 0;
+
+    int.TryParse(d, out e); // if can convert 'd', values pass to 'e'
+    Console.WriteLine(e);
+    Console.WriteLine("Conversão realizada!");
+
+}
+
+void switchCase(){
+    Console.WriteLine("Digite uma vogal:");
+    string? letra = Console.ReadLine();
+
+    switch(letra)
+    {
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+            Console.WriteLine("Vogal");
+            break;
+        default:
+            Console.WriteLine("Não é uma vogal");
+            break;
+
+    }
+
+}
+
+void tabuadaX(){
+    Console.WriteLine("Digite o numero da tabuada:");
+    string? x = Console.ReadLine();
+    int defaultNumber = 0;
+    int.TryParse(x, out defaultNumber); 
+    for (int i = 0; i <= 10; i++) // index
+    {
+        Console.WriteLine($"{defaultNumber} x {i} = {defaultNumber * i}");
+    }
+}
+
+void doWhile(){
+    int soma = 0, numero = 0;
+    do
+    {
+        Console.WriteLine("Digite um numero (0 para parar)");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        soma += numero;
+    } while (numero != 0);
+
+    Console.WriteLine($"Total da soma dos numeros digitados é {soma}");
+}
+
+void menu(){
+    string opcao;
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine("Digite a sua opção");
+        Console.WriteLine("1 - Cadastrar cliente");
+        Console.WriteLine("2 - Buscar cliente");
+        Console.WriteLine("3 - Apagar cliente");
+        Console.WriteLine("4 - Encerrar");
+
+        opcao = Console.ReadLine();
+
+        switch(opcao)
+        {
+            case "1":
+                Console.WriteLine("Cadastro de cliente");
+                break;
+            case "2":
+                Console.WriteLine("Busca de cliente");
+                break;
+            case "3":
+                Console.WriteLine("Deletar de cliente");
+                break;
+            case "4":
+                Console.WriteLine("Encerrar");
+                Environment.Exit(0); // exit of while true
+                break;
+            default:
+                Console.WriteLine("Opção invalida");
+                break;
+        }
+    }
+}
+
+DateTimeHowToUse();
+ConvertTypes();
+switchCase();
+tabuadaX();
+doWhile();
+menu();
 
 // void UsarVarTypes(){
 //     varTypes.Altura = 1.80; // double
--- obj/Debug/net8.0/DOTNET_1rst_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/DOTNET_1rst_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DOTNET_1rst_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c69d07028ed511af771268f39691727bccddb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fba60c67fa6f7e04ad88d38880401116474c2a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DOTNET_1rst_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DOTNET_1rst_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
